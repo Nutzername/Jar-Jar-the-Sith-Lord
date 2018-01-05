1b9f017bab9bc2432c6fdec054c6c687d9c6b099
Nutzername <steam_konto@gmx.de>
2018-01-05 16:04:39 +0100

    Add files via upload

---
+++ js/levels.js
@@ -0,0 +1,57 @@
+var levels = [
+    {
+    },
+    {
+        name:"NABOO",
+        theme:'jarjar',
+        background: '#111133',
+        template:[
+            "                                        C                                                                                     ",
+            "    B                                                                                                                         ",
+            "                          A                                                                                C                  ",
+            "            C                  D   pcc        cccc      ccc      pccc   D                  B                       123        ",
+            "  123                A             kkkk      WWWWWW    WWWWW     zzzzz                                             456        ",
+            "  456      p  c  p                WHHHHW      °°°°      °°°       °°°      zzzzz                                              ",
+            "           WWWWWWW                 f  f       °°°°    C °°°  A    °°°       °°°        ~             D                        ",
+            "             lll          y        f  f       °kk°      °°°   B   °°°    y  °°°     kkkkk       A   C                         ",
+            "             lll b       U     A   f  f     WWkkkkWW    °°°       °°°       °°° EFG  f f                                      ",
+            "             lll      kkkkkk       f  f  WWWkkkkkkkkWWW °°°       °°°       °°°kkkk  f f            B                         ",
+            "         kkkkkll       f  f        f  fWWkkkkkkkkkkkkkkWk°°       °°°       °°°° f   f f                                   I  ",
+            "          °°°lll       f  f        f  f  fccf °°°° fccf °°°       °°°    zzz°°°° f   f f                          {}c888  II  ",
+            "      y   °°°lll   k   f  f        f pfp fccf °°°° fccfp°°°       °°°    lll°°°° f   f f                       {=}88888888gggg",
+            "          °°°lll   f   f  f        f kkkkkkkkkkkkkkkkkkkkkk       °zzzz  lll°°°° f   f f                WWW   8888888888gggggg",
+            "   ^      °°°lll   f   f  f        f  f  f  T °PQ° T  f °°°       °lPQT  lll°°°° f   f f                °e°  8888ggggggggggggg",
+            "=}/ü`{}   °°°lll   f   f  f        fp f  f  T °RS° T  f °°° {=}   °lRST plll°°°° f Upf f            p   °e° 8888888888888ggg88",
+            "gggxxxxxxxxxxxxx@@@8@@@8@@8@@@@@@@@8888888888888888888888888ggggg888888888888888888888888@@@@@@@@@888888888888888888888ggggggg"
+        ]
+    },
+    {},
+    {
+        name:"SCC",
+        theme:'jarjar',
+        background: '#001b14',
+        template:[
+            "JJ                                                                                  JJJJJ        A                              ",
+	    "JJJ                                                                               JJJJJJJJ                                      ",
+            "JJ                                            A            C              ~        JJJJJJJJ                                     ",
+            "K                ([           123                             123         I         JJJJJ                                       ",
+            "K         C      )]           456                             456         I          JJJ      D                   C             ",
+            "K                                                                         I           K                                         ",
+            "K  y   A         D                                   D                    I           K                 B                       ",
+            "K  ^     B         I                                                      I         cpK                                         ",
+            "K /ü`           I  II                  I                                  I C      JJJJ                                         ",
+            "xxxxx          IIJJJJJJ     J    J    II y                                I           K                        A                ",
+            "xxxx        JJJJJJJJJJJJJ   I    I    kkkkkkk                 B           I           K                                         ",
+            "xxx        JJJJJJJJJJJJJ    I  J I    xcc  f                              I           K  c                                      ",
+            "xx            JJJJJJJJ      I  I I    xcc  f                     I        I   ~       JJJ        B                              ",
+            "xx               JKJJ       I  I I    xxxxxxx                    I        I   I       K                                         ",
+            "xx                KJ        x  I I   IxxxI             I         I        I   I       K                          µµµµµ          ",
+            "x            xx   K         x  I I   xxxxxI            I         I    ~   I   I       K y                       µµ              ",
+            "x           xxxx  K         x  I x  IxxxxxxI I         I     ~   I    I   I   I       K          I              µµ              ",
+            "x           xx xx K        xx  I x  xxxxxxxx I    I    I     I   I    I   I   I       K x    I   I              µµLMµµ          ",
+            "x           x   xxK        xx  Ixx  xxxxxxxxIII I I    I     I   I    I   I   I      xxxxx   I   I    x     EFG µµNOµµ          ",
+            "x           x   xxxx       xx  xxx  xxxxxxxxIII I I p  I     I   I    I   I   I     xxxxxxx  I   I   xx     gggggggggggggggggggg",
+            "x@@@@@@@@@@@x@@@xxxxx@@@@@@xx@@xxx@@xxxxxxxxxxxxxxxxxxxxxxxxx@@@@@@@@@@@@@@@@@@@@@@xxxxxxxxxxxxxxxxxxxx      xxxxxxxxxxxxxxxxxxx"
+        ]
+    }
+];
